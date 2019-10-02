09f6141b36a0262052d0e88a0ca025169b4231a6
dachosen1 <anderson.nelson1@gmail.com>
2019-10-02 13:31:52 -0400

    undo name changes

--- Sample File.R
+++ Sample File.R
@@ -5,17 +5,5 @@ data <- read.csv('Data/iris.csv')
 plot_data <- plot(data)
 
 
-=======
-# Anderson 
-print('Anderson')
->>>>>>> 0772d7880213c1cab259ba67ca3aa2b9322ed4ee
-=======
-#Kristina
-print('Kristina')
->>>>>>> bc57bb1629a6a48c96d8e8a7cd662ba9b9b2a423
 
->>>>>>> bc57bb1629a6a48c96d8e8a7cd662ba9b9b2a423
-
-#Print Name
-print("Luis")
 
